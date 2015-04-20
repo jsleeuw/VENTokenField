79f05a4f9beeec1918574dd146a39bdf58b60620
Jonathan Sleeuw <jsleeuw@gmail.com>
2015-04-20 16:36:44 -0400

    token font

--- VENTokenField/VENToken.m
+++ VENTokenField/VENToken.m
@@ -52,6 +52,7 @@
 {
     self.titleLabel.text = text;
     self.titleLabel.textColor = self.colorScheme;
+    self.titleLabel.font = [UIFont fontWithName:@"AvenirNext-DemiBold" size:16];
     [self.titleLabel sizeToFit];
     self.frame = CGRectMake(CGRectGetMinX(self.frame), CGRectGetMinY(self.frame), CGRectGetMaxX(self.titleLabel.frame) + 3, CGRectGetHeight(self.frame));
     [self.titleLabel sizeToFit];
